4f03f9235823f145ba14f06b2501148ac7a39c51
AdelaideTh <athompson23@bfhsla.org>
2020-09-29 17:20:13 +0000

    * add makeBody method

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 (function(window, _) {
   window.myLibraryName = window.myLibraryName || {
     numz: {},
-    phyz: {},
-  };
+    phyz: {
+    
+    makeBody: function(type, {
+        velocityX = 0,
+        velocityY = 0,
+        rotaionalVeocity = 0,
+        integrity = 1,
+        density = 1,
+        volatility = 0
+    } = {}) {
+    if (type === undefined) throw new Error("You must provide a valid string for the type parameter");
+    return {
+        type: type,
+        velocityX: velocityX,
+        velocityY: velocityY,
+        rotaionalVeocity: rotaionalVeocity,
+        integrity: integrity,
+        density: density,
+        volatility: volatility,
+
+        handleCollision(impact, body){
+
+        },
+
+        update(event){
+
+        }
+    };
+    },
+    },
+};
 }(window, window._));
