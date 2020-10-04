380d1cd3ab5f03ffd092cc1aa3d4ddd067eb207d
AdelaideTh <athompson23@bfhsla.org>
2020-10-04 23:39:19 +0000

    *add several functions

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 (function(window, _) {
-  window.myLibraryName = window.myLibraryName || {
-    numz: {},
+  window.sporc = window.sporc || {
+    numz: {
+        getDistance: function(pointA, pointB) {
+            const
+                distanceX = pointB.x - pointA.x;
+                distanceY = pointB.y - pointA.y;
+                distance = math.sqrt(distanceX * distanceX + distanceY * distanceY);
+                return distance;
+        },
+        getAngleDegrees: function(pointA, pointB){
+            const
+                distanceX = pointB.x - pointA.x,
+                distanceY = pointB.y - pointA.y,
+                radians = Math.atan2(distanceX, distanceY),
+                degrees = radians * 180 / math.PI;
+                return degrees;
+        }
+    },
     phyz: {
     
     makeBody: function(type, {
